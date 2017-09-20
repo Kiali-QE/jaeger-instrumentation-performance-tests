4f12a9d38c5d310b383ca6a6fb01e9757b027925
Kevin Earls <kevin@kevinearls.com>
2017-09-20 13:49:40 +0200

    Changed package names to start with org.hawkular.qe instead of com.kevinearls

---
+++ vertx/src/main/java/org/hawkular/qe/vertx/util/BackendService.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.qe.vertx.util;
+
+import io.opentracing.ActiveSpan;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.vertx.ext.web.TracingHandler;
+import io.vertx.ext.web.RoutingContext;
+
+import java.util.logging.Logger;
+
+public class BackendService {
+    public static Logger logger = Logger.getLogger(BackendService.class.getName());
+    private TracingHandler tracingHandler;
+    private Tracer tracer;
+
+    public BackendService(Tracer tracer, TracingHandler tracingHandler) {
+        this.tracingHandler = tracingHandler;
+        this.tracer = tracer;
+    }
+
+    public void action(RoutingContext routingContext) throws InterruptedException {
+        SpanContext spanContext = tracingHandler.serverSpanContext(routingContext);
+        try (ActiveSpan span = tracer.buildSpan("action").asChildOf(spanContext).startActive()) {
+            anotherAction();
+        }
+    }
+
+    private void anotherAction() {
+        ActiveSpan activeSpan = tracer.activeSpan();
+        if (activeSpan != null) {
+            activeSpan.setTag("anotherAction", "data");
+        } else {
+            logger.info("tracer.activeSpan returned null");
+        }
+    }
+}
---
+++ wildfly-swarm/src/main/java/org/hawkular/qe/wildflyswarm/TracingContextListener.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.qe.wildflyswarm;
+
+import com.uber.jaeger.metrics.Metrics;
+import com.uber.jaeger.metrics.NullStatsReporter;
+import com.uber.jaeger.metrics.StatsFactoryImpl;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.reporters.Reporter;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
+import com.uber.jaeger.samplers.Sampler;
+import com.uber.jaeger.senders.Sender;
+import com.uber.jaeger.senders.UdpSender;
+import io.opentracing.NoopTracerFactory;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@WebListener
+public class TracingContextListener implements ServletContextListener {
+    private static Map<String, String> envs = System.getenv();
+
+    private static Integer JAEGER_FLUSH_INTERVAL = new Integer(envs.getOrDefault("JAEGER_FLUSH_INTERVAL", "100"));
+    private static Integer JAEGER_MAX_PACKET_SIZE = new Integer(envs.getOrDefault("JAEGER_MAX_PACKET_SIZE", "0"));
+    private static Integer JAEGER_MAX_QUEUE_SIZE = new Integer(envs.getOrDefault("JAEGER_MAX_QUEUE_SIZE", "50"));
+    private static Double JAEGER_SAMPLING_RATE = new Double(envs.getOrDefault("JAEGER_SAMPLING_RATE", "1.0"));
+    private static Integer JAEGER_UDP_PORT = new Integer(envs.getOrDefault("JAEGER_UDP_PORT", "5775"));
+    private static String JAEGER_AGENT_HOST = envs.getOrDefault("JAEGER_AGENT_HOST", "localhost");
+    private static final String TRACER_TYPE = envs.getOrDefault("TRACER_TYPE", "jaeger");
+    private static final String TEST_SERVICE_NAME = envs.getOrDefault("TEST_SERVICE_NAME", "wildfly-swarm-opentracing-demo");
+    private static Logger logger = Logger.getLogger(TracingContextListener.class.getName());
+
+    @Inject
+    private io.opentracing.Tracer tracer;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        GlobalTracer.register(tracer);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {}
+
+    @Produces
+    @Singleton
+    public static io.opentracing.Tracer jaegerTracer() {
+        Tracer tracer;
+
+        if (TRACER_TYPE.equalsIgnoreCase("jaeger")) {
+            logger.info("Using JAEGER tracer using host [" + JAEGER_AGENT_HOST + "] port [" + JAEGER_UDP_PORT +
+                    "] Service Name " + TEST_SERVICE_NAME + " Sampling rate " + JAEGER_SAMPLING_RATE);
+
+            Sender sender = new UdpSender(JAEGER_AGENT_HOST, JAEGER_UDP_PORT, JAEGER_MAX_PACKET_SIZE);
+            Metrics metrics = new Metrics(new StatsFactoryImpl(new NullStatsReporter()));
+            Reporter reporter = new RemoteReporter(sender, JAEGER_FLUSH_INTERVAL, JAEGER_MAX_QUEUE_SIZE, metrics);
+            Sampler sampler = new ProbabilisticSampler(JAEGER_SAMPLING_RATE);
+            tracer = new com.uber.jaeger.Tracer.Builder(TEST_SERVICE_NAME, reporter, sampler)
+                    .build();
+        } else {
+            logger.info("Using NOOP Tracer");
+            tracer = NoopTracerFactory.create();
+        }
+
+        return tracer;
+    }
+}
