ee3800b82049101a484caf9f3a10fddea744deb8
Kevin Earls <kevin@kevinearls.com>
2017-10-09 10:13:12 +0200

    First attempt at hacky cassandra workaround

--- common/src/test/java/org/hawkular/qe/common/ValidateTracesIT.java
+++ common/src/test/java/org/hawkular/qe/common/ValidateTracesIT.java
@@ -3,6 +3,7 @@ package org.hawkular.qe.common;
 import com.datastax.driver.core.*;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -10,10 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 public class ValidateTracesIT {
     private static Map<String, String> envs = System.getenv();
@@ -49,16 +47,45 @@ public class ValidateTracesIT {
     @Test
     public void testCountTraces()  throws IOException {
         Session session = cluster.connect(KEYSPACE_NAME);
-        ResultSet result = session.execute(QUERY);
-        Row one = result.one();
-        long traceCount = one.getLong(0);
-        logger.info(">>>>> TRACE COUNT: " + traceCount);
+        long minStartTime = getAggregateValue(session, "select min(start_time) from traces ALLOW FILTERING");
+        long maxtartTime = getAggregateValue(session, "select max(start_time) from traces ALLOW FILTERING");
+        logger.info("MIN " + minStartTime + " MAX " + maxtartTime);
 
+        long totalTraceCount = 0;
+        long startTime = minStartTime;
+        while (startTime <= maxtartTime) {
+            String query = "select count(*) from traces where  start_time=" + startTime + " ALLOW FILTERING;";
+            long traceCountForMs = getAggregateValue(session, query);
+            logger.info("AT " + startTime + " got " + traceCountForMs);
+            startTime +=1000;  // TODO confirm we only store down to ms
+            totalTraceCount += traceCountForMs;
+        }
+
+        logger.info(">>>>> TRACE COUNT: " + totalTraceCount);
         // FIXME At some point we need to pass in the expected trace count.  In the short term write it to a file.  Then
         // We can have the JenkinsFIle add it to the description of the job
-        Files.write(Paths.get("traceCount.txt"), Long.toString(traceCount).getBytes(), StandardOpenOption.CREATE);
+        Files.write(Paths.get("traceCount.txt"), Long.toString(totalTraceCount).getBytes(), StandardOpenOption.CREATE);
     }
 
+    /**
+     * This method is to simplify aggregate queries.
+     *
+     * @param session The Cassandra session
+     * @param query Text of the query
+     * @return single long result of the query: COUNT, MAX, MIN, etc.
+     */
+    private long getAggregateValue(Session session, String query) {
+        ResultSet result = session.execute(query);
+        Row one = result.one();
+        BigInteger stupid = one.getVarint(0);
+        logger.info("Query [" + query + "] returned [" + stupid.longValue() + "]");
+        return stupid.longValue();
+    }
+
+
+
+
+    @Ignore
     @Test
     public void testListKeyspaces() {
         Cluster.Builder builder = Cluster.builder();
