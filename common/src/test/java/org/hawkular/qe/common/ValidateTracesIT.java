2e0346809296cd131bc90cae7c4dea7fbb8ce66c
Kevin Earls <kevin@kevinearls.com>
2017-10-09 12:13:26 +0200

    Fix double counting issue, attempt 2

--- common/src/test/java/org/hawkular/qe/common/ValidateTracesIT.java
+++ common/src/test/java/org/hawkular/qe/common/ValidateTracesIT.java
@@ -80,9 +80,9 @@ public class ValidateTracesIT {
         long interval = 1_000_000L;  // Trace start_time is in microseconds, so this should get 1 sec at a time.
         while (startTime <= (maxStartTime + 1)) {
             long endTime = startTime + interval;
-            String query = "select count(*) from traces where  start_time >" + startTime + " AND start_time < " + endTime + " ALLOW FILTERING;";
+            String query = "select count(*) from traces where  start_time >=" + startTime + " AND start_time <" + endTime + " ALLOW FILTERING;";
             long currentTraceCount = getAggregateValue(session, query);
-            startTime +=interval;
+            startTime = endTime;
             totalTraceCount += currentTraceCount;
         }
 
