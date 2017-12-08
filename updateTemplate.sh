2012b31c312a12bedd432479e8d9699007d155f2
Kevin Earls <kearls@redhat.com>
2017-12-08 12:10:00 +0100

    Add definition for COLLECTOR_PODS parameter

--- updateTemplate.sh
+++ updateTemplate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -x
 sed -i 's;parameters:.*$;\0\n- description: Queue size parameter for the collector\n  displayName: Jaeger Collector Queue Size\n  name: COLLECTOR_QUEUE_SIZE\n  required: false\n  value: "300000";g' jaeger-production-template.yml
+sed -i 's;parameters:.*$;\0\n- description: Number of collector pods\n  displayName: Jaeger Collector Pods\n  name: COLLECTOR_PODS\n  required: false\n  value: "1";g' jaeger-production-template.yml
+
 sed -i 's;.*- "--config-file=/conf/collector.yaml".*$;\0\n            - "--collector.queue-size=${COLLECTOR_QUEUE_SIZE}";g' jaeger-production-template.yml
 #
 # HACK alert.  Change the number of replicas for the collector here if desired.  This script currently depends
